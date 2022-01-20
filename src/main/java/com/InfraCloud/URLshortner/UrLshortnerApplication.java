a6e1b01acd188e8ef2c1fd253f0d86645542f067
Vidyadhar97 <Vidyadharkotyal@gmail.com>
2022-01-20 14:53:20 +0530

    Inital Commit

---
+++ src/main/java/com/InfraCloud/URLshortner/UrLshortnerApplication.java
@@ -0,0 +1,13 @@
+package com.InfraCloud.URLshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLshortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrLshortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InfraCloud/URLshortner/UrLshortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InfraCloud.URLshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLshortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
